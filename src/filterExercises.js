4d06596fe3aae6dadfd3a50b980b3a28b0dd39d4
David Darden <davidedarden83@gmail.com>
2021-03-31 11:19:30 -0500

    initial commit

--- src/filterExercises.js
+++ src/filterExercises.js
@@ -2,33 +2,77 @@ import { useStudents, useInstructors } from "./data/classroom.js";
 
 const students = useStudents();
 const instructors = useInstructors();
-
+console.log("DAVID")
 // Export a function named getStudentsInCohort
 // It should accept one integer parameter named `cohort`
 // It should return an array of just the students who are in that cohort
+//--------------------------------------------
+//  export let getStudentsInCohort = (cohort) => {
+//  let specCohort = useStudents.cohort.filter(b => cohort === cohort)
+//  if (b = true){
+//  return specCohort}
+//  }
+//--------------------------------------------
+// export let getStudentsInCohort = useStudents.filter(function (e){
+// })
+//----------------------------------------------
+//debugger
+//let specCohortArray = [];
+//for(let index=0; index < students.length; index++)
+//---------------------------------------------
+export const getStudentsInCohort = (cohort) => {
+    const filteredStudents = students.filter(studentObj => studentObj.cohort === cohort)
+    return filteredStudents
+}
+console.log(getStudentsInCohort(43))
 
 // Export a function called getFullTimeStudents
 // It should not accept any parameters
 // It should return an array of only the full time students
-
+export const getFullTimeStudents = () => {
+    const filteredStudents = students.filter(studentObj => studentObj.fullTime === true)
+    return filteredStudents
+}
 // Export a function called getStudentsByInstructorId
 // It should accept one integer parameter name `instructorId`
 // It should return an array of students with that instructor
-
+export const getStudentsByInstructorId = (instructorId) => {
+    const filteredStudents = students.filter(studentObj => studentObj.instructorId === instructorId)
+    return filteredStudents
+}
 // Export a function called getPolyglotStudents
 // It should accept one integer parameter named `languageCount`
 // It should return an array of students who know as many (or more) languages than `languageCount`
 // Ex: If the number 2 is passed to the function, only the students who know 2 or more languages should be returned
+export const getPolyglotStudents = (languageCount) => {
+    const filteredStudents = students.filter(studentObj => studentObj.languages.length >= languageCount)
+    return filteredStudents
+}
+console.log(getPolyglotStudents(2))
 
 // Export a function called getAvailableInstructors
 // It should not accept any parameters
 // It should return an array of instructors that don't have any students
+export const getAvailableInstructors = () => {
+    let studentArray = useStudents().map(item =>{
+        return item.instructorId;
+    })
+    return useInstructors().filter(item =>{
+        if (studentArray.includes(item.id) === false){
+            return item;
+        }
+    })
+} 
+console.log("getAvailableInstructors test:")
+console.log(getAvailableInstructors())
 
 // Export a function called getStudentsByLanguage
 // It should accept one string parameter named `language`
 // It should return an array of students who know the given language
 // HINT: In addition to the `filter` method, you might also look up the `some` method
-
+export const getStudentsByLanguage = () => {
+    
+}
 /******** ADVANCED CHALLENGE ********/
 /******** Only do this if all other tests are passing ****/
 /******** To test, uncomment the code at the bottom of tests/filter.spec.js  *****/
--- src/findExercises.js
+++ src/findExercises.js
@@ -7,28 +7,56 @@ const instructors = useInstructors();
 // It should accept one integer parameter named `id`
 // It should return the student object with the matching ID
 // Ex: getStudentById(1)
-
+export const getStudentById = (id) => {
+    return students.find(studentObj => studentObj.id === id)
+}
+console.log("getStudentById test:")
+console.log(getStudentById(1))
 // Export a function called getInstructorById
 // It should accept one integer parameter named `id`
 // It should return the instructor object with the matching ID
 // Ex: getInstructorById(1)
-
+export const getInstructorById = (id) => {
+    return instructors.find(instructorObj => instructorObj.id === id)
+}
+console.log("getInstructorById test:")
+console.log(getInstructorById(1))
 // Export a function called getStudentByLastName
 // It should accept one string parameter named `lastName`
 // It should return the student object whose last name matches `lastName`
 // It should NOT be case sensitive
 // Ex: getStudentByName("sMiTh")
-
+export const getStudentByLastName = (lastName) => {
+    return students.find(studentObj => studentObj.lastName === lastName)
+}
+console.log("getStudentByLastName test:")
+console.log(getStudentByLastName("Smith"))
 // Export a function called getStudentByName
 // It should accept one string parameter named `fullName`
 // It should return the student object whose first and last name match `fullName`
 // It should NOT be case sensitive
 // Ex: getStudentByName("Summer SMITH")
+export const getStudentByName = (fullName) => {
+    return students.find(studentObj => studentObj.firstName + " " + studentObj.lastName === fullName)
+}
+console.log("getStudentByName test:")
+console.log(getStudentByName("Summer Smith"))
 
 // Export a function called getInstructorOfStudent
 // It should accept one integeter parameter named `studentId`
 // It should return the instructor object of the student whose id matches `studentId`
 // Ex: getInstructorOfStudent(4)      // returns Brenda Long
+
+//working HERE!!!!!!!!!!!!!!!!!!
+export const getInstructorOfStudent = (studentId) => {
+ //  console.log(students)
+    let stud = students.find(studentObj => studentObj.id === studentId)
+    console.log(stud)
+    let studentInstructor= stud.instructorId
+    return instructors.find(instructorObj => instructorObj.id === studentInstructor )
+}
+console.log("getInstructorsOfStudents test:")
+console.log(getInstructorOfStudent(4))
 
 // Export a function called getStudentWithMostLangs
 // It should not accept any parameters
